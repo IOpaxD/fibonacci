88219abdeb040b7cf7723f9cda35addb51aad614
Yura-kenig <Yura-kenig@yandex.ru>
2019-12-10 23:15:40 +0300

    добавлен ввод с клавиатуры

--- src/main/java/org/example/FibCalc.java
+++ src/main/java/org/example/FibCalc.java
@@ -4,9 +4,31 @@ package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class FibCalc {
+
+    public static void main(String[] args) throws IOException {
+        FibCalc fibCalc = new FibCalc();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Для выхода введите любую букву");
+        int i;
+        while (true) {
+            System.out.print("Введите число:  ");
+            try {
+                i = Integer.parseInt(reader.readLine());
+            } catch (NumberFormatException s) {
+                System.out.println("Выход");
+                break;
+            }
+            System.out.println(fibCalc.calc(String.valueOf(i)));
+        }
+        reader.close();
+    }
+
     private Logger log = LogManager.getLogger();
     public final static BigInteger[][] ZERO = {{BigInteger.ONE, BigInteger.ZERO}, {BigInteger.ZERO, BigInteger.ONE}};
     public final static BigInteger[][] ONE = {{BigInteger.ZERO, BigInteger.ONE}, {BigInteger.ONE, BigInteger.ONE}};
@@ -32,14 +54,13 @@ public class FibCalc {
         long l = Long.parseLong(s);
         if (l < 0) {
             l = -(l);
-            if (l % 2== 0) {
+            if (l % 2 == 0) {
                 b = pow(ONE, l)[0][1];
                 i = b.multiply(BigInteger.valueOf(-1));
-                return  i.toString();
+                return i.toString();
             }
         }
         b = pow(ONE, l)[0][1];
         return b.toString();
-
     }
 }
